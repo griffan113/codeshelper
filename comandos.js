8368afe8612aed5976ba0fefa69b5248897f9edc
Eliel <70238970+griffan113@users.noreply.github.com>
2020-09-13 17:09:59 -0300

    Update and rename utilidades.md to comandos.js

---
+++ comandos.js
@@ -0,0 +1,44 @@
+Alguns comandos do BOT:
+
+⋙ Entreterimento: ⋘
+
+➜ coin - Joga cara ou coroa; 
+➜ roastme - Dá uma lição de moral (te xinga);
+➜ meavise - Você será avisado com esse comando;
+➜ rlshop - Mostra a rotação de itens da loja do Rocket League;
+➜ avatar - Mostra o seu avatar ou mencione alguem para ver o avatar da pessoa;
+➜ meme - Manda um meme aleatório do Reddit;
+➜ rps - Joga Pedra, Papel, Tesoura;
+➜ nada - Faz simplesmente nada!
+
+⋙ Utilidades: ⋘
+
+➜ sv - Link do servidor;
+➜ userinfo - Mostra algumas informações de determinado usuário;
+➜ serverinfo - Mostra as informações do servidor;
+➜ ping - Mostra o ping do servidor;
+➜ cargo - Escolha um cargo especial;
+➜ cor - Escolha uma cor específica;
+➘ cor remova - Remove a cor;
+➜ feedback - Envia uma sugestão á algum canal de feedback existente.
+
+⋙ Comandos avançados do Bot: ⋘
+
+➜ recruitment - Dá início ao processo de recrutamento para Staff;
+➜ event - Entra para algum evento existente no servidor;
+➜ c - Mostra alguns comandos na linguagem C/C++;
+➜ py - Mostra alguns comandos na linguagem Python.
+Em breve mais comandos...
+
+⋙ Comandos da Staff: ⋘
+
+➜ aviso - Envia uma mensagem de aviso á algum canal definido;
+➜ clear - Limpa uma quantidade de mensagens;
+
+➘ Comandos de Moderação:
+
+➜ kick;
+➜ mute;
+➜ unmute;
+➜ ban;
+➜ warn;
--- utilidades.md
+++
@@ -1,3 +0,0 @@
-⋙ Utilidades: ⋘
-
-➜ sv - Link do servidor; ➜ userinfo - Mostra algumas informações de determinado usuário; ➜ serverinfo - Mostra as informações do servidor; ➜ ping - Mostra o ping do servidor; ➜ cargo - Escolha um cargo especial; ➜ cor - Escolha uma cor específica; ➘ cor remova - Remove a cor; ➜ feedback - Envia uma sugestão á algum canal de feedback existente.
